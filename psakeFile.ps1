82a17afe716c02c59ee29ae9bc9f0813bf76693e
Joey Piccola <joey.piccola@gmail.com>
2020-04-29 20:43:37 -0600

    set PSB preferences and add UploadTestResults task

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,10 +1,16 @@
 properties {
-    # Disable "compiling" module into monolithinc PSM1.
-    # This modifies the default behavior from the "Build" task
-    # in the PowerShellBuild shared psake task module
-    $PSBPreference.Build.CompileModule = $false
+    # vars to support psake task UploadTestResults used in appveyor.yml
+    $testResultsPath = Join-Path -Path $PSScriptRoot -ChildPath "output/$env:BHProjectName/testResults.xml"
+    $PSBPreference.Test.OutputFile = $testResultsPath
+    # other PowerShellBuild preferences
+    $PSBPreference.Build.CompileModule = $true
 }
 
 task default -depends Test
 
 task Test -FromModule PowerShellBuild -Version '0.4.0'
+
+task UploadTestResults {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $testResultsPath)
+} -description 'Uploading tests'
